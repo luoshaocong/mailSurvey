4526afadae8c5dd31cde969cb666e06161ba689f
luoshaocong <tigaluo@gmail.com>
2018-11-19 23:42:28 -0500

    fix bug

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,5 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COKKIE_KEY
+  cookieKey: process.env.COOKIE_KEY
 };
-mongodb://tiga:cA-199403@ds011288.mlab.com:11288/email-prod
