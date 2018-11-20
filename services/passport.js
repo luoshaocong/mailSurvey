9b3b0866bd0ae3bf6cd4b0a1f1fa5d6c310c95bd
luoshaocong <tigaluo@gmail.com>
2018-11-20 00:46:40 -0500

    tweak google strategy setting

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,10 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+
+      //relative path .http no https
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     (accessToken, refreshToken , profile ,done) => {
       User.findOne({ googleId : profile.id}).then(existingUser =>{
