e37d43af9cb10a3601e8899a26b6771bce357ce2
luoshaocong <tigaluo@gmail.com>
2018-11-22 00:17:00 -0500

    finish log in

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,29 +1,40 @@
-import React from 'react';
+import React, {Component } from 'react';
 import { BrowserRouter, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 
-const Header = () => <h2>Header</h2>
+import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
 
-const App = () => {
-  return (
-    <div>
-      <BrowserRouter>
-        <div>
-        <Header />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/surveys" component={Dashboard} />
-        <Route  path="/surveys/new" component={SurveyNew} />
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+
+  }
+  render(){
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route  path="/surveys/new" component={SurveyNew} />
 
 
-        </div>
-      </BrowserRouter>
+          </div>
+        </BrowserRouter>
 
-    </div>
-  );
+      </div>
+    );
+
+  }
+
 };
-
-export default App;
+//first argument reserve for mapstatetoprops
+//
+export default connect(null, actions)(App);
