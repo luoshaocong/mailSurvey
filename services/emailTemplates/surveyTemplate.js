e4cffa55bbcec513c6a4f4f9c92b5b20dc630baf
luoshaocong <tigaluo@gmail.com>
2018-11-28 23:30:37 -0500

    finish survey

--- services/emailTemplates/surveyTemplate.js
+++ services/emailTemplates/surveyTemplate.js
@@ -1,4 +1,27 @@
+const keys = require('../../config/keys');
 module.exports = (survey) => {
-  return  '<div>'+survey.body+ '</div>';
+  return `
+    <html>
+      <body>
+        <div style="text-align: center;">
+          <h3> you !</h3>
+          <p> answer my question</p>
+          <p>${survey.body}</p>
+          <div>
+            <a href="${keys.redirectDomin}/api/surveys/thanks">Yes</a>
+          </div>
+          <div>
+            <a href="${keys.redirectDomin}/api/surveys/thanks">Yes</a>
+          </div>
+
+        </div>
+
+      </body>
+
+    </html>
+
+
+
+  `;
 
 };
