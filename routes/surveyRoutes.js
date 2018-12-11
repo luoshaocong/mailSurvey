705dc9f4f687aae1fb92934f739c3cf8bfd84230
luoshaocong <tigaluo@gmail.com>
2018-12-11 16:35:31 -0500

    filter surveyID choice and email  in webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,4 +1,6 @@
-
+const _ = require('lodash');
+const Path = require('path-parser').default;
+const { URL } = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -13,6 +15,24 @@ module.exports = app => {
     res.send('Thank you for voting !');
   });
 
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const events = _.map(req.body, ({email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path('/api/surveys/:surveyId/:choice');
+      const match = p.test(pathname);
+      if(match) {
+        return {email, surveyId: match.surveyId, choice: match.choice};
+      }
+    });
+
+    // get rid of undeined
+    const compactEvents = _.compact(events);
+    //eliminate dupbicate  email and survey id
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+    console.log(uniqueEvents);
+    res.send({});
+
+  });
 
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     //es6 restructuring.
